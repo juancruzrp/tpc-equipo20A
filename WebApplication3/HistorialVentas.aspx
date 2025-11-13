2fd74a62f1c107c31d2944bd9d312c4559ef210e
juancruzrp <soycruzjuan@hotmail.com>
2025-11-12 23:55:19 -0300

    Ventas

--- WebApplication3/HistorialVentas.aspx
+++ WebApplication3/HistorialVentas.aspx
@@ -1,6 +1,11 @@
 ﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="HistorialVentas.aspx.cs" Inherits="WebApplication3.HistorialVentas" %>
 <asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
 
+    <asp:Label ID="LabelBuscar" runat="server" Text="Buscar Venta:" ></asp:Label>
+    <asp:TextBox ID="txtBuscar" runat="server" placeholder="Ingrese Fecha..." CssClass="form-control" Width="300px" />
+    <br />
+
+    <asp:GridView ID="dgvVentas" runat="server" CssClass="table table-striped table-bordered" ></asp:GridView>
 
 
 </asp:Content>
--- WebApplication3/HistorialVentas.aspx.cs
+++ WebApplication3/HistorialVentas.aspx.cs
@@ -1,9 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using Negocio;
+using Dominio;
 
 namespace WebApplication3
 {
@@ -11,6 +15,15 @@ namespace WebApplication3
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (!IsPostBack)
+                cargarVentas();
+        }
+
+        private void cargarVentas()
+        {
+            VentaNegocio negocio = new VentaNegocio();
+            dgvVentas.DataSource = negocio.listarVentas();
+            dgvVentas.DataBind();
 
         }
     }
